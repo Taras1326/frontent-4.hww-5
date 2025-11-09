1c086365a7c7f1f2236286f332fcf065eb74d2e4
Taras1326 <166243411+Taras1326@users.noreply.github.com>
2025-11-09 21:18:05 +0200

    Updates

--- frontent-4.hww-5.0f77c784.js
+++ frontent-4.hww-5.0f77c784.js
@@ -207,11 +207,11 @@
       });
     }
   }
-})({"jIJTx":[function(require,module,exports,__globalThis) {
+})({"7wZbQ":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 53486;
+var HMR_SERVER_PORT = 1234;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "439701173a9199ea";
 var HMR_USE_SSE = false;
@@ -17418,6 +17418,6 @@ exports.export = function(dest, destName, get) {
     });
 };
 
-},{}]},["jIJTx","2R06K"], "2R06K", "parcelRequire4bc5", {})
+},{}]},["7wZbQ","2R06K"], "2R06K", "parcelRequire4bc5", {})
 
 //# sourceMappingURL=frontent-4.hww-5.0f77c784.js.map
